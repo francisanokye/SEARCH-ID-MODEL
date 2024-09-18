94f9ab7f80dcfbfd7614d7dc7bc52719e0e3f32c
Francis Anokye <francis.anokye@aims-senegal.org>
2024-09-18 11:03:31 -0230

    updates

--- searchid/macpan2/codes/searchi/calibrate.R
+++ searchid/macpan2/codes/searchi/calibrate.R
@@ -25,7 +25,7 @@ calibrator <- mp_tmb_calibrator(
   data = seroprevdata,
   traj = "cases",
   outputs = c("cases",outputs),
-  par = c("beta_values", "gamma", "phi","mu", "xi")#, "theta", "omega", "alpha") 
+  par = c("beta_values","mu")#,"gamma","mu")#, "phi","mu", "xi", "theta", "omega", "eta") 
 )
 
 mp_optimize(calibrator)
@@ -51,89 +51,68 @@ backtrans <- function(x) {
 
 coeff <- mp_tmb_coef(calibrator, conf.int = TRUE) |> backtrans()
 print(coeff)
-
-
 ############ Visualize simulation with calibrated paramters against the observed data ###########
+
 
 plot_fit = function(cal_object) {
   fitted_data = mp_trajectory_sd(cal_object)
-  fitted_data$dates <- seq.Date(from = as.Date("2021-12-01"), by = "day", length.out = nrow(fitted_data))
-  fitted_data <- fitted_data[fitted_data$dates > "2021-12-14",]
-  seroprevdata <- seroprevdata[seroprevdata$dates > "2021-12-14",]
-  ggplot(seroprevdata, aes(x = dates, y = value, colour = "black"), na.rm = TRUE) +
-    geom_point(size = 2) +
-    geom_line(aes(x = dates, y = value,colour = "red"), data = fitted_data, size = 1.5) +
-    # geom_ribbon(aes(x = dates, ymin = conf.low, ymax = conf.high), data = fitted_data, alpha = 0.2, fill = "red") +
-    labs(x = "Date (Dec 2021  - June 2022)", y = "Omicron True Infections", title = "SEARCHI Model Fit With macpan2", color = "") +
-    scale_color_manual(labels = c("data", "fit"),values = c("black","red")) +
-    #annotate("text", x = as.Date("2021-12-18"), y = 700, label = expression(beta[2] == 0.20),size = 6,angle = 90, hjust = 1, color = "black")+
-  #annotate("text", x = as.Date("2021-12-28"), y = 700, label = expression(beta[3] == 5.21),size = 6,angle = 90, hjust = 1,color = "black")+
-  #annotate("text", x = as.Date("2022-01-30"), y = 550, label = expression(beta[4] == 0.62),size = 6,angle = 0, hjust = 1, color = "black")+
-  # annotate("text", x = as.Date("2022-02-05"), y = 3, label = "Alert Level 3",size = 5,angle = 10, hjust = 1, color = "black")+
-  #annotate("text", x = as.Date("2022-03-03"), y = 550, label = expression(beta[3] == 0.70),size = 6,angle = 0, hjust = 1, color = "black")+
-  #annotate("text", x = as.Date("2022-05-01"), y = 1500, label = expression(beta[0] == 0.82),size = 6, hjust = 1, color = "black")+
-    theme_clean() +
-    geom_vline(xintercept = as.Date("2022-03-18"), colour = "purple", linetype = 6, size = 1)  +
-    #geom_vline(xintercept = as.Date("2022-04-10"), colour = "purple", linetype = 6, size = 2)  +
-    geom_vline(xintercept = as.Date("2021-12-23"), colour = "gold4", linetype = 2, size = 1)  +
-    geom_vline(xintercept = as.Date("2022-01-03"), colour = "gold4", linetype = 2, size = 1)  +
-    geom_vline(xintercept = as.Date("2022-02-06"), colour = "gold4", linetype = 2, size = 1)  +
+  # Define the start date
+  start_date <- as.Date("2021-12-15")
+  # Generate a sequence of dates based on the 'time' column
+  fitted_data$dates <- start_date + fitted_data$time - 1
+  print(head(fitted_data,20))
+  # Check the number of unique values in the 'matrix' column
+  unique_values_matrix <- length(unique(fitted_data$matrix))
+  
+  p <- ggplot(data = fitted_data, aes(x = dates, y = value)) +
+    geom_line(aes(color = factor(matrix)), linewidth = 1.5) +  # Ensure 'matrix' is a valid factor
+    geom_point(data = seroprevdata, aes(x = dates, y = value, color = "data"), size = 2) +
+    #geom_vline(aes(xintercept = x), linetype = "dashed",color = "gold4", alpha = 0.95,size = 1.1, data = data.frame(x = beta_changepoints)) +
+    labs(x = "Date (Dec 2021 – June 2022)", y = "Incidence", title = "SEARCHI Model Fit: Incidence Trajectory (macpan2)", color = "") +
+    theme_clean()+
+    facet_wrap(~matrix, scales = "free") +
+    #annotate("text", x = as.Date("2021-12-18"), y = 200, label = expression(beta[2] == 0.20),size = 1,angle = 90, hjust = 1, color = "black")+
+    #annotate("text", x = as.Date("2021-12-28"), y = 200, label = expression(beta[3] == 5.21),size = 1,angle = 90, hjust = 1,color = "black")+
+    #annotate("text", x = as.Date("2022-01-30"), y = 250, label = expression(beta[4] == 0.62),size = 1,angle = 0, hjust = 1, color = "black")+
+    #annotate("text", x = as.Date("2022-02-05"), y = 3, label = "Alert Level 3",size = 5,angle = 10, hjust = 1, color = "black")+
+    #annotate("text", x = as.Date("2022-03-03"), y = 250, label = expression(beta[3] == 0.70),size = 1,angle = 0, hjust = 1, color = "black")+
+    #annotate("text", x = as.Date("2022-05-01"), y = 200, label = expression(beta[0] == 0.82),size = 1, hjust = 1, color = "black")+
+    geom_vline(xintercept = as.Date("2022-03-18"), colour = "purple", linetype = 4, size = 1)  +
+    geom_vline(xintercept = as.Date("2021-12-23"), colour = "gold4", linetype = 4, size = 1)  +
+    geom_vline(xintercept = as.Date("2022-01-03"), colour = "gold4", linetype = 4, size = 1)  +
+    geom_vline(xintercept = as.Date("2022-02-06"), colour = "gold4", linetype = 4, size = 1)  +
     geom_vline(xintercept = as.Date("2022-03-14"), colour = "gold4", linetype = 1, size = 1)  +
-    annotate("text", x = as.Date("2022-03-05"), y = 2200, label = "Pre-Cancellation of Public \nHealth Emergency Declaration",size=4, hjust=1, color = "darkblue")+
-    annotate("text", x = as.Date("2022-05-20"), y = 2200, label = "Post Cancellation of Public \nHealth Emergency Declaration",size=4, hjust=1,color = "darkblue")+
-    theme(axis.text.x = element_text(size = 20, angle = 45, hjust = 1),
-          axis.title.x = element_text(size = 20, color = "black", face = "bold"),
-          axis.text.y = element_text(size = 20),
-          axis.title.y = element_text(size = 20, color = "black", face = "bold"),
+    theme(axis.text.x = element_text(size = 10, angle = 45, hjust = 1),
+          axis.title.x = element_text(size = 15, color = "black", face = "bold"),
+          axis.text.y = element_text(size = 10),
+          axis.title.y = element_text(size = 15, color = "black", face = "bold"),
           plot.title = element_text(size = 18, face = "bold", color = "black", hjust = 0.5),
-          legend.position = c(0.80, 0.35),
-          legend.title = element_text(size = 20),
-          legend.text = element_text(size = 15),
+          legend.position = c(0.85, 0.10),
+          legend.title = element_text(size = 15),
+          legend.text = element_text(size = 10),
           legend.background = element_rect(color = NA),
           legend.margin = margin(0, 0, 0, 0),
           plot.background = element_blank()) +
-          theme(plot.title = element_text(hjust = 0.5))
-	}
+    theme(plot.title = element_text(hjust = 0.5))
+  
+  # Conditionally apply scale_color_manual based on the number of unique values in 'matrix'
+  if (unique_values_matrix == 1) {
+    p <- p + scale_color_manual(labels = c("fit", "data"), values = c("red", "black"))
+    p <- p + theme(legend.position = c(0.85, 0.35))
+  } else {
+    p <- p + scale_color_manual(labels = c("A", "C", "fit", "data", "E", "H", "I", "R", "S"),
+                                values = c("#008080", "blue", "red", "black", "purple", "orange", "navy", "green", "brown"))
+  }
+  
+  print(p)
+}
 
-plot_seroprevalence = function(cal_object) {
-  fitted_data = mp_trajectory_sd(cal_object)
-  fitted_data$dates <- seq.Date(from = as.Date("2021-12-15"), by = "day", length.out = nrow(fitted_data))
-  fitted_data <- fitted_data[fitted_data$matrix == "R",]
-  print(fitted_data)
-  #fitted_data$R <- fitted_data$R / population
-  fitted_data <- fitted_data[(fitted_data$dates > "2021-12-14")& (fitted_data$dates < "2022-06-04"),]
-  seroprevdata <- seroprevdata[(seroprevdata$dates > "2021-12-14") &(seroprevdata$dates < "2022-06-04"),]
-  ggplot(seroprevdata, aes(x = date, y = daily_serop, colour = "CITF Seroprevalence"), na.rm = TRUE) +
-  	geom_point(data = fitted_data, aes(x = dates, y = value, color = "Recovery Per Capita"), size = 1) +
-  	scale_color_manual(name = NULL,values = c("Recovery Per Capita" = "darkgreen","CITF Seroprevalence" = "brown")) +
-  	labs(x = "Date", y = "Seroprevalence", title = "Recovery Per Capita and CITF Seroprevalence", color = "") +
-  	geom_vline(xintercept = as.Date("2022-03-18"), colour = "purple", linetype = 6, size = 1)  +
-        geom_vline(xintercept = as.Date("2021-12-23"), colour = "gold4", linetype = 2, size = 1)  +
-        geom_vline(xintercept = as.Date("2022-01-03"), colour = "gold4", linetype = 2, size = 1)  +
-        geom_vline(xintercept = as.Date("2022-02-06"), colour = "gold4", linetype = 2, size = 1)  +
-        geom_vline(xintercept = as.Date("2022-03-14"), colour = "gold4", linetype = 1, size = 1)  +
-        annotate("text", x = as.Date("2022-03-05"), y = 0.8, label = "Pre-Cancellation of Public \nHealth Emergency Declaration",size=4, hjust=1, color = "darkblue")+
-        annotate("text", x = as.Date("2022-05-20"), y = 0.8, label = "Post Cancellation of Public \nHealth Emergency Declaration",size=4, hjust=1,color = "darkblue")+
-	theme_clean() +
-  	theme(axis.text.x = element_text(size = 20, angle = 45, hjust = 1),
-        	axis.title.x = element_text(size = 20, color = "black", face = "bold"),
-        	axis.text.y = element_text(size = 20),
-        	axis.title.y = element_text(size = 20, color = "black", face = "bold"),
-        	plot.title = element_text(size = 20, face = "bold", color = "black", hjust = 0.5),
-        	legend.position = c(0.25, 0.75),
-        	legend.title = element_text(size = 20),
-        	legend.text = element_text(size = 20),
-        	legend.background = element_rect(color = NA),
-        	legend.margin = margin(0, 0, 0, 0),
-        	plot.background = element_blank()) +
-        theme(plot.title = element_text(hjust = 0.5))	
-	}
 
 plot_fit(calibrator)
 
-plot_seroprevalence(calibrator)
 
 rdsSave(calibrator)
+
 
 
 
