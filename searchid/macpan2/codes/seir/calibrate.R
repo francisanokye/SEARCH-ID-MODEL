06032dfecb6c60557ca0cc39a38ac38ea4b7f5a4
wzmli <wzmichael.li@gmail.com>
2024-10-09 17:01:11 -0400

    Done cleaning for steve?

--- searchid/macpan2/codes/seir/calibrate.R
+++ searchid/macpan2/codes/seir/calibrate.R
@@ -21,24 +21,12 @@ outputs = c("S", "E", "I", "R", "cases","sero_cases","serop")
 population = 510550
 
 
-#prior_distributions = list(
-#      beta = beta
-#    , logit_report_prob = qlogis(1.0)
-#    , logit_serop_frac = qlogis(0.95)
-#    #, "log_E0","log_I0","log_R0"
-#)
-
-
 calibrator <- mp_tmb_calibrator(
-  spec = timevar_spec #|> mp_rk4()
+  spec = timevar_spec 
   , data = seroprevdata
-#  , traj = c("cases", "serop","sero_cases")
-  , traj = c("cases","serop")# ,"sero_cases")
+  , traj = c("cases","serop")
   , outputs = c(outputs)
-# , par = c("beta","log_I0")#,"report_prob") ## ,"serop_frac")#,"log_R0")
- , par = c("beta_values","log_I0","report_prob") ## ,"serop_frac")#,"log_R0")
-#  , par = c("beta_values","log_I0") # ,"report_prob") ## ,"serop_frac")#,"log_R0")
-#  , time = mp_sim_bounds(-60, 432, "daily")
+  , par = c("beta_values","log_I0") 
   , time = mp_sim_bounds(0, 432, "daily")
 )
 
