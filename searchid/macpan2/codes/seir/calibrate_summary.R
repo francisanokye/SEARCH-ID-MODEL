681c6499a233a360c85446d1aa5d82344f1b7d64
stevencarlislewalker <stevencarlislewalker@gmail.com>
2024-10-11 10:15:24 -0400

    getting closer

--- searchid/macpan2/codes/seir/calibrate_summary.R
+++ searchid/macpan2/codes/seir/calibrate_summary.R
@@ -15,24 +15,8 @@ calibrator <- rdsRead()
 
 print(calibrator)
 ##########################################################################################
-# see here, https://github.com/canmod/macpan2/issues/179
-backtrans <- function(x) {
-  vars1 <- intersect(c("default", "estimate", "conf.low", "conf.high"), names(x))
-  prefix <- stringr::str_extract(x[["mat"]], "^log(it)?_")  |> tidyr::replace_na("none")
-  sx <- split(x, prefix)
-  for (ptype in setdiff(names(sx), "none")) {
-    link <- make.link(stringr::str_remove(ptype, "_"))
-    sx[[ptype]] <- (sx[[ptype]]
-                    |> mutate(across(std.error, ~link$mu.eta(estimate)*.))
-                    |> mutate(across(any_of(vars1), link$linkinv))
-                    |> mutate(across(mat, ~stringr::str_remove(., paste0("^", ptype))))
-    )
-  }
-  bind_rows(sx)
-}
-##########################################################################################
 
-coeff <- mp_tmb_coef(calibrator, conf.int = TRUE) |> backtrans()
+coeff <- mp_tmb_coef(calibrator, conf.int = TRUE)
 print(coeff)
 ############ Visualize simulation with calibrated paramters against the observed data ###########
 
