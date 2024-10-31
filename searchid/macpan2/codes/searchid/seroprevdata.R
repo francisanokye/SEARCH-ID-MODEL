0019ea96f74661b5cc5aa49730a32677030ab310
wzmli <wzmichael.li@gmail.com>
2024-10-31 00:30:57 -0400

    Autocommit

--- searchid/macpan2/codes/searchid/seroprevdata.R
+++ searchid/macpan2/codes/searchid/seroprevdata.R
@@ -4,7 +4,7 @@ library(tidyverse)
 
 start_date <- "2021-12-14"
 last_date <- "2022-06-01"
-trim_report <- "2022-05-01"
+trim_report <- "2022-04-01"
 
 sero <- (csvRead() 
 	|> mutate(dates = as.Date(date,format = "%Y-%m-%d"))
